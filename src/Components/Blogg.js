b0ffb7016fc325e8187d698970893853737c7996
tejaswini22199 <tejaswini22199@gmail.com>
2021-01-02 19:59:04 +0530

    blogs done

--- src/Components/Blogg.js
+++ src/Components/Blogg.js
@@ -2,21 +2,23 @@ import React from 'react';
 import axios from 'axios';
 import {useState,useEffect} from 'react';
 import Displayblogs from './Displayblogs';
+import Searchbar from './Searchbar';
 const Blogg = () => {
     const [blog,setBlog]=useState([]);
+    const [query,setQuery]=useState('');
     useEffect(() => {
        const fetchItems=async()=>
        {
-           const response=await axios('https://dev.to/api/articles?username=powercoder');
+           const response=await axios(`https://dev.to/api/articles?username=${query}`);
            setBlog(response.data);
            console.log(response.data);
        }
-     
-      fetchItems();
-    },[]);
-    return (
         
+      fetchItems();
+    },[query]);
+    return (
         <div>
+            <Searchbar getQuery={(q)=>setQuery(q)}/>
             <Displayblogs blog={blog}/>
         </div>
     )
--- src/Components/Displayblogs.js
+++ src/Components/Displayblogs.js
@@ -1,12 +1,34 @@
 import React from 'react'
 import BlogDescription from './BlogDescription'
+import Loader from './Loader'
+import '../App.css'
 const Displayblogs = ({blog}) => {
+    var u,t,g;
+    if(blog==='undefined' || blog.length===0)
+    return <Loader/>;
+    if( blog !== 'undefined' && blog.length>0){
+    u=blog[0].user;
+    t="https://twitter.com/"+u.twitter_username;
+    g="https://github.com/"+u.github_username;
+    console.log(t);
+    }
     return (
         <div>{
             blog.map((element,index) => {
                return <BlogDescription key={index} element={element}/>
             })
         }
+         <div className="nav-foot">
+            
+            <h2>{u.name}</h2>
+            <img src={u.profile_image} alt="none"/>
+            <div className="atags">
+            <a href={t}><i class="fab fa-twitter"></i></a>
+            <a href={g}><i class="fab fa-github"></i></a>
+            <a href="u.website_url"><i class="fas fa-globe"></i></a>
+            </div>
+            </div>
+         
         </div>
     )
 }
