87491d1afaf1350c0fee22de4a09f9c51ec6f5eb
rakshaa2000 <rakshaa2000@gmail.com>
2021-01-03 14:47:20 +0530

    Better UI

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -7,6 +7,7 @@ const Home = () => {
                 <h1>Dev-Folio</h1>
                 <h2>The next-gen Developer Portfolio</h2>
                 <img src={logo} alt="Logo" width="400px" height="400px"></img>
+                <h3>One stop place to check all spheres</h3>
             </center>
             
         </div>
