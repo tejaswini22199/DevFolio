906cc2f7c053cb2557ff2b63faf22e7d09641e16
rakshaa2000 <rakshaa2000@gmail.com>
2021-01-03 12:41:16 +0530

    homepage

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -3,6 +3,10 @@ import React from 'react'
 const Home = () => {
     return (
         <div>
+            <center>
+                <h1>Dev-Folio</h1>
+                <h2>The next-gen Developer Portfolio</h2>
+            </center>
             
         </div>
     )
